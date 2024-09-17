c8ea1b0e86124b23ec1df8bd2e9aa2ae1e1a1b34
aidan <arfiedler1114@gmail.com>
2024-09-17 13:06:43 -0500

    add actual blurbs to the website, yayyyy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,15 +13,15 @@
     <div style="margin: 3%;">
         <div class="box">
             <div class="image_left"><enhanced:img src='$lib/assets/IMG_3829.jpg' alt='tmp logo' class="images"/></div>
-            <div class="text_right">lorem ipsum dolor amit somethign somwthing something something ;djfn lasjdfnl nasfdakjsdfasn asdfkjskfn asldf</div>
+            <div class="text_right">We are Raven Robotics, an FRC team that is located at Francis Howell High in St. Charles, Missouri. We have been competing for over 20 years and continue to grow each season</div>
         </div>
         <div class="box">
-            <div class="text_left">lorem ipsum dolor amit somethign somwthing something something ;djfn lasjdfnl nasfdakjsdfasn asdfkjskfn asldf</div>
+            <div class="text_left">While our main mission is to create a robot to compete in each FRC season, our goal at Raven Robotics is to expand students knowledge of engineering, teamwork, and communication in order to create future leaders in the engineering field. We focus on three main components in the engineering field when building our robots: mechanical, electrical, and programming.</div>
             <div class="image_right"><enhanced:img src='$lib/assets/IMG_3582.jpg' alt='tmp logo' class="images"/></div>
         </div>
         <div class="box">
             <div class="image_left"><enhanced:img src='$lib/assets/IMG_3542.jpg' alt='tmp logo' class="images"/></div>
-            <div class="text_right">lorem ipsum dolor amit somethign somwthing something something ;djfn lasjdfnl nasfdakjsdfasn asdfkjskfn asldf</div>
+            <div class="text_right">Raven Robotics has had many successes throughout our 20 years of operating. We have won 16 rewards, and competed at FRC World Championship 5 times. In the 2024 FRC season, our team got 1st place at the St. Louis regionals event and got 13th in our division at the 2024 FRC World Championship.</div>
         </div>
     </div>
 </body>
@@ -54,6 +54,7 @@
         margin: 3%;
         width: 30%;
         justify-content: center;
+        align-items: center;
         float: left;
         display: flex;
         flex-grow: 1;
@@ -62,6 +63,7 @@
         margin: 3%;
         width: 30%;
         justify-content: center;
+        align-items: center;
         float: right;
         display: flex;
         flex-grow: 1;
